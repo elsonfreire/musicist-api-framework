f05800cbf9f925acb8f46dfbf0e6e25a3ef4344c
elsonfreire <elsonfreirefilho@gmail.com>
2026-05-11 13:14:55 -0300

    Remove unused imports

--- src/main/java/br/com/musicist/modules/goals/controller/GoalController.java
+++ src/main/java/br/com/musicist/modules/goals/controller/GoalController.java
@@ -16,16 +16,12 @@ import br.com.musicist.modules.user.model.User;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 
 @RestController
