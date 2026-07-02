5a0dcef8c2a0806c8e85f92cb7163b40b8048908
Thiago Cordeiro <thiagoty52@gmail.com>
2026-07-01 21:27:09 -0300

    feat: delete auditable entity

--- src/main/java/br/com/habit/modules/framework/shared/model/UserOwnedEntity.java
+++ src/main/java/br/com/habit/modules/framework/shared/model/UserOwnedEntity.java
@@ -1,16 +1,29 @@
 package br.com.habit.modules.framework.shared.model;
 
 import br.com.habit.modules.framework.user.model.User;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @MappedSuperclass
 @Getter
-public abstract class UserOwnedEntity extends AuditableEntity {
+public abstract class UserOwnedEntity {
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    private UUID id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     protected User user;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
